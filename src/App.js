6463835db3e1c287486e64297768242de2739734
Thomas Sypniewski <tsypniew@fr-b-tb04915.local>
2017-10-18 18:17:28 +0200

    add onClick animation with tweenmax & sort apps by date

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import apps from './datas';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import TweenMax from 'gsap';
 import './App.css';
 
+// Feature : onClick displays more informations
 class AppRow extends React.Component {
 
   constructor(props) {
@@ -13,11 +14,48 @@ class AppRow extends React.Component {
     this.changeRow = this.changeRow.bind(this);
   }
 
-  changeRow(e) {
-    console.log('the tr was clicked');
-    this.setState(prevState => ({
-      isTrClicked: !prevState.isTrClicked
-    }));
+/* Don't forget to put this in the CSS
+  tbody tr div {
+    overflow: hidden;
+  }
+*/
+
+  changeRow() {
+    const tr = this.refs.tr; // Get DOM Element
+    // Get infos about tr. try : document.body.getBoundingClientRect();
+    const rect = tr.getBoundingClientRect();
+    console.log('RECT tr', rect);
+
+    // Allow to stop the animation (for example in case of multiple clicks)
+    TweenMax.killTweensOf(tr);
+
+    if(this.state.isTrClicked) {
+      TweenMax.to(tr, 1, {
+        // Here we cheat a bit as we already now the height of the div
+        height: 30,
+        onComplete: () => {
+          this.setState({
+            isTrClicked: !this.state.isTrClicked
+          });
+        }
+      })
+    }
+    else {
+      tr.style.height = rect.height+'px'; // DOM property element
+      // setState can have a callback
+      this.setState({
+        isTrClicked: !this.state.isTrClicked
+      }, () => {
+        tr.style.height = '';
+        const newRect = tr.getBoundingClientRect();
+
+        TweenMax.fromTo(tr, 1, {
+          height: rect.height
+        },{
+          height: newRect.height
+        });
+      });
+    }
   }
 
   render() {
@@ -26,51 +64,97 @@ class AppRow extends React.Component {
     const env = app.environment;
     const date = app.date;
 
-    return(
-      <tr onClick={this.changeRow}
-          style=
-            {
-              this.state.isTrClicked ?
-              {backgroundColor:"#ff5050"} :
-              {backgroundColor:null}
-            }
-      >
+    // We change the typeof "date = app.date" from string to date object
+    // This allow us to compare the date with the currentDate - new DateTime
+    var dateCompare = date.split('/').reverse().join('-');
+    dateCompare = new Date(dateCompare);
 
-        <td>{name[0].fdqn}</td>
-        <td>{env}</td>
-        <td>{date}</td>
-      </tr>
+    let clicked = this.state.isTrClicked;
+
+    let domainNames = name.slice(1).map((element, index) => {
+      return (
+        <span key={index}>{element.fdqn}<br/></span>
+      )
+    })
+
+    return(
+       clicked ?
+         <tr onClick={this.changeRow}>
+           <td>
+             {/* Set ref */}
+             {/* We set a div around as we had problems with table properties */}
+            <div ref="tr">
+             {name[0].fdqn} <br/>
+             {domainNames}
+            </div>
+           </td>
+           <td>{env}</td>
+           {new Date() > dateCompare ?
+             <td style={{backgroundColor:"red"}}>{date}</td> :
+             <td>{date}</td>
+           }
+         </tr>
+      :
+        <tr onClick={this.changeRow}>
+          <td>
+            {/* Set ref */}
+            <div ref="tr">
+              {name[0].fdqn}
+            </div>
+          </td>
+          <td>{env}</td>
+          {new Date() > dateCompare ?
+            <td style={{backgroundColor:"red"}}>{date}</td> :
+            <td>{date}</td>
+          }
+        </tr>
     )
   }
 }
 
 // Displays and filters the data collection based on user input
 class AppsTable extends React.Component {
+
+  sortAppByDate(arr) {
+    const now = Date.now();
+    return arr.sort((a, b) => {
+      const dateA = new Date(a.date.split('/').reverse().join('-'));
+      const dateB = new Date(b.date.split('/').reverse().join('-'));
+
+      // On transforme en TimeStamp
+      const diffA = now - dateA.getTime();
+      const diffB = now - dateB.getTime();
+
+      if(diffA < 0 && diffB >= 0) {
+        return 1;
+      }
+      else if(diffB < 0 && diffA >= 0) {
+        return -1;
+      }
+      else {
+        return dateA > dateB;
+      }
+    });
+  }
+
   render() {
 
     const env = this.props.env;
 
     const rows = [];
 
+
     // Find a better way to this conditionnal rendering
     if (env === 'all') {
-      this.props.apps.map((app, index) => {
+      this.sortAppByDate(this.props.apps).map((app, index) => {
         return rows.push(
           <AppRow
             app={app} key={index} env={env}/>
         );
       })
-    } else if (env === 'stagging'){
+    } else {
       // Add toLowerCase() method
-      this.props.apps.filter(app => app.environment === 'stagging').map(
-        (app, index) => {
-          return rows.push(
-            <AppRow
-              app={app} key={index} env={env}/>
-          );
-        })
-    } else {  // si env == 'prod'
-      this.props.apps.filter(app => app.environment === 'prod').map(
+      this.sortAppByDate(this.props.apps.filter(app => app.environment === env)).map(
         (app, index) => {
           return rows.push(
             <AppRow
@@ -78,7 +162,6 @@ class AppsTable extends React.Component {
           );
         })
     }
-
 
     return (
       <table>
@@ -109,11 +192,13 @@ class DropdownList extends React.Component {
   render() {
 
     return (
-      <select value={this.props.env} onChange={this.handleEnv}>
-        <option value="all">All</option>
-        <option value="stagging">Developpement</option>
-        <option value="prod">Production</option>
-      </select>
+      <div className="chooseEnv">
+        <select value={this.props.env} onChange={this.handleEnv}>
+          <option value="all">All</option>
+          <option value="stagging">Developpement</option>
+          <option value="prod">Production</option>
+        </select>
+      </div>
     )
   }
 }
@@ -122,51 +207,51 @@ class DropdownList extends React.Component {
 ** CSS EXAMPLE  ***
 ******************/
 
-class CssExample extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {items : ['click', 'to', 'add', 'item']};
-    this.handleAdd = this.handleAdd.bind(this);
-  }
-
-  handleAdd(){
-    const newItems = this.state.items.concat([
-      prompt('Enter some text')
-    ]);
-    this.setState({items: newItems});
-  }
-
-  handleRemove(i){
-    let newItems = this.state.items.slice();
-    newItems.splice(i, 1);
-    this.setState({items : newItems});
-  }
-
-  render(){
-
-    console.log(this.state.items);
-
-    const items = this.state.items.map((item, index) => {
-      return (
-        <div key={item} onClick={() => this.handleRemove(index)}>
-          {item}
-        </div>
-      )
-    });
-
-    return (
-      <div>
-        <button onClick={this.handleAdd}>Add Item</button>
-        <ReactCSSTransitionGroup
-          transitionName="example"
-          transitionEnterTimeout={1000}
-          transitionLeaveTimeout={500}>
-          {items}
-          </ReactCSSTransitionGroup>
-      </div>
-    );
-  }
-}
+// class CssExample extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {items : ['click', 'to', 'add', 'item']};
+//     this.handleAdd = this.handleAdd.bind(this);
+//   }
+//
+//   handleAdd(){
+//     const newItems = this.state.items.concat([
+//       prompt('Enter some text')
+//     ]);
+//     this.setState({items: newItems});
+//   }
+//
+//   handleRemove(i){
+//     let newItems = this.state.items.slice();
+//     newItems.splice(i, 1);
+//     this.setState({items : newItems});
+//   }
+//
+//   render(){
+//
+//     console.log(this.state.items);
+//
+//     const items = this.state.items.map((item, index) => {
+//       return (
+//         <div key={item} onClick={() => this.handleRemove(index)}>
+//           {item}
+//         </div>
+//       )
+//     });
+//
+//     return (
+//       <div>
+//         <button onClick={this.handleAdd}>Add Item</button>
+//         <ReactCSSTransitionGroup
+//           transitionName="example"
+//           transitionEnterTimeout={1000}
+//           transitionLeaveTimeout={500}>
+//           {items}
+//           </ReactCSSTransitionGroup>
+//       </div>
+//     );
+//   }
+// }
 
 /******************
 ** FIN CSS EXAMPLE  ***
@@ -193,7 +278,7 @@ class FilterableAppsTable extends React.Component {
   render() {
     return (
       <div>
-        <CssExample />
+        {/* <CssExample /> */}
 
         <DropdownList
           env={this.state.env}
--- src/datas.js
+++ src/datas.js
@@ -15,37 +15,43 @@
 var apps = [
   {
     environment: "prod",
-    name:[{fdqn:"www.rugby.fr"},{fdqn:"rugby.fr"},{fdqn:"www.rugby.fr"}],
+    description : "Lorem ipsum dolor sit amet, consectetur adipi",
+    name:[{fdqn:"www.rugby.fr"},{fdqn:"rugby.fr"},{fdqn:"www.rugby.com"}],
     date: "27/02/2018"
   },
 
   {
     environment: "stagging",
+    description : "o laboris nisi ut aliquip ex luptate veli",
     name: [{fdqn:"foot.com"},{fdqn:"www.foot.fr"},{fdqn:"www.foot.fr"}],
     date: "27/02/2017"
   },
 
   {
     environment: "prod",
+    description : "Lorem ipsum dolor sit amet, consectetur adipi",
     name: [{fdqn:"www.tennis.fr"},{fdqn:"tennis.fr"},{fdqn:"www.tennis.com"}],
     date: "18/10/2017"
   },
 
   {
     environment: "stagging",
+    description : "o laboris nisi ut aliquip ex luptate veli",
     name: [{fdqn:"www.peche.fr"},{fdqn:"peche.fr"},{fdqn:"www.peche.com"}],
     date: "30/10/2017"
   },
 
   {
     environment: "prod",
+    description : "Lorem ipsum dolor sit amet, consectetur adipi",
     name: [{fdqn:"www.pong.fr"},{fdqn:"pong.fr"},{fdqn:"www.pong.com"}],
     date: "22/10/2016"
   },
 
   {
     environment: "stagging",
-    name: [{fdqn:"www.rocket.fr"},{fdqn:"rocket.fr"},{fdqn:"www.rocket.com"}], 
+    description : "Lorem ipsum dolor sit amet, consectetur adipi",
+    name: [{fdqn:"www.rocket.fr"},{fdqn:"rocket.fr"},{fdqn:"www.rocket.com"}],
     date: "22/11/2016"
   }
 ];
